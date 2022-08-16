918bf5c39cd0b4198cd4d48a6fe50c2036e4bc68
AhmedBinYasin <ahmed116046@gmail.com>
2022-08-16 15:42:22 +0500

    Prepairation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import LoginScreen from './pages/LoginScreen';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import { useState } from 'react';
-
 
 function App() {
-  const [res, setRes] = useState('');
-  const [Email, setEmail] = useState('');
-  const [sat, setSat] = useState('1');
-  const setToUserLogin=()=>{
-    setSat('1')
-  }
-  const setToUserRegister=()=>{
-    setSat('2')
-  }
-  const setToHome=()=>{
-    setSat('3')
-  }
-  const setNewRes=(responseData)=>{
-    setRes(responseData.user)
-  }
-  const getEmail=(email)=>{
-    setEmail(email)
-  }
-  switch (sat) {
-    case '1':
-      document.title='User Login'
-      return (
-        <>
-          <LoginScreen set={setToUserRegister} sethome={setToHome} setRes={setNewRes} getEmail={getEmail}></LoginScreen>
-        </>
-      );
-    case '2':
-      document.title='User Registration'
-      return (
-        <>
-          <Register set={setToUserLogin}></Register>
-        </>
-      );
-      case '3':
-      document.title='Home'
-      return (
-        <>
-          <Home Data={res} Email={Email} Role={res.Roll}></Home>
-        </>
-      );
-  
-    default:
-      break;
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
